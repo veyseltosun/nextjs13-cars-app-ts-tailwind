f97c22d7b0f36a91e1fed8759cdd755fdca7e138
veyseltosun <postalarim1923@gmail.com>
2023-07-05 22:40:53 +0300

    ...

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,8 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
+import  SearchManufacturer from "./SearchManufacturer";
 
 
 
-export {Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter} ;
+export {Hero, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer} ;
