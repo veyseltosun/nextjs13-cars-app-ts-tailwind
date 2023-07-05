86bbda5b9dafad10c827cd39bd349149f7d0c469
veyseltosun <postalarim1923@gmail.com>
2023-07-05 22:55:32 +0300

    props search manufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,20 @@
 "use client";
-import SearchManufacturer from "./SearchManufacturer";
+import {  useState } from "react";
+import {SearchManufacturer} from "./";
 
 const SearchBar = () => {
+
+    const [manufacturer, setManufacturer] = useState("");
+
+
     const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className='searchbar__item'>
-            <SearchManufacturer/>
+            <SearchManufacturer
+             manufacturer={manufacturer}
+             setManufacturer={setManufacturer}
+            />
 
         </div>
     </form>
