9ff20676e74862864a31891b0b86f7cf41a49f22
veyseltosun <postalarim1923@gmail.com>
2023-07-08 11:34:06 +0300

    ...

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -67,7 +67,26 @@ const SearchManufacturer = ({manufacturer,
                                  relative search-manufacturer__option ${active ? "bg-primary-blue text-white" : "text-gray-900"}`}
                                  value={item}
                                 >
-                                    {item}
+                 {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                           
+                          </span>
+                        ) : null}
+                      </>
+                    )}
                                 </Combobox.Option>
                             ))
                         )}
