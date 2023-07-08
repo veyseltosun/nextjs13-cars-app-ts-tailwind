38e056b0a2155b05f50df46db6c4f6c11f424392
veyseltosun <postalarim1923@gmail.com>
2023-07-08 19:21:18 +0300

    modal style

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -63,7 +63,18 @@ const CardDetails = ({isOpen, closeModal, car} : CarDetailsProps) => {
               <div className='flex-1 flex flex-col gap-3'>
 
                 <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
-
+                <Image src="/hero.png"   alt="car model" fill priority className="object-contain"/>
+                </div>
+                <div className='flex gap-3'>
+                  <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                  <Image src="/hero.png"   alt="car model" fill priority className="object-contain"/>
+                  </div>
+                  <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                  <Image src="/hero.png"   alt="car model" fill priority className="object-contain"/>
+                  </div>
+                  <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                  <Image src="/hero.png"   alt="car model" fill priority className="object-contain"/>
+                  </div>
                 </div>
               </div>
             </Dialog.Panel>
