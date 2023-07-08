10fe600b2b5baf5d03e498248400327a7def72f6
veyseltosun <postalarim1923@gmail.com>
2023-07-08 14:16:11 +0300

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+
+
 import Image from 'next/image'
 import { Hero, SearchBar, CustomFilter } from '@/components'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
+
   return (
     <main className="overflow-hidden">
       <Hero/>
@@ -19,6 +26,16 @@ export default function Home() {
           </div>
 
         </div>
+        {!isDataEmpty ? (
+          <section>
+            WE HAVE CARS
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   )
--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,15 @@
-const fetch = require('node-fetch');
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': '81b4a225b9msh47e6d4f147d3fc2p177408jsn80bd9a6c5bce',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+        
+    };
 
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '81b4a225b9msh47e6d4f147d3fc2p177408jsn80bd9a6c5bce',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
-};
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+        headers : headers,
+    })
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    const result = await response.json();
+    return result;
+    
 }
